8af5e243f79139bbf2d77c5cb08a45aa4baf2954
ZhangGang <2862714434@qq.com>
2024-10-14 22:29:43 +0800

    delete wrong name file

--- SerachEngine/src/ConfigTest.cc
+++
@@ -1,9 +0,0 @@
-#include "Configuration.hh"
-
-int main(int argc, char* argv[]) {
-    Configuration::getConfigInstance()->printMap();
-    Configuration::getConfigInstance()->printSet();
-
-    return 0;
-}
-
